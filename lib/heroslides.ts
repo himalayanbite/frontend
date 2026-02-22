552b15e7c307902be55663655f273e2d38dfe5ac
Aananda-git <bhusal.aananda1@gmail.com>
2026-02-22 21:49:55 +0200

    feat: add heroslides constant for hero images in the carousel

---
+++ lib/heroslides.ts
@@ -0,0 +1,19 @@
+import { SlideItem } from "@/types/SlideItem.types";
+
+export const heroslides:SlideItem[] = [
+  {
+    id: 1,
+    image: "/discount.jpeg",
+    title: "",
+  },
+  {
+    id: 2,
+    image: "/hero2.jpg",
+    title: "Fresh Ingredients Daily",
+  },
+  {
+    id: 3,
+    image: "/hero3.jpg",
+    title: "Experience Nepali Cuisine",
+  },
+]
