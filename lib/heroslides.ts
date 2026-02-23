a3a8052e7730321772e53aaba6857584d2240ab3
Aananda-git <bhusal.aananda1@gmail.com>
2026-02-23 21:20:20 +0200

    feat: update heroslides to use correct image path

--- lib/heroslides.ts
+++ lib/heroslides.ts
@@ -3,17 +3,17 @@ import { SlideItem } from "@/types/SlideItem.types";
 export const heroslides:SlideItem[] = [
   {
     id: 1,
-    image: "/discount.jpeg",
+    image: "/heroslider2.png",
     title: "",
   },
-  {
-    id: 2,
-    image: "/hero2.jpg",
-    title: "Fresh Ingredients Daily",
-  },
-  {
-    id: 3,
-    image: "/hero3.jpg",
-    title: "Experience Nepali Cuisine",
-  },
+  // {
+  //   id: 2,
+  //   image: "/hero2.jpg",
+  //   title: "Fresh Ingredients Daily",
+  // },
+  // {
+  //   id: 3,
+  //   image: "/hero3.jpg",
+  //   title: "Experience Nepali Cuisine",
+  // },
 ]
