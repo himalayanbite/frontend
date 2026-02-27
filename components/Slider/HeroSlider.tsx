f684df5bad8ab941522df0aa605244d0a2d595d1
Aananda-git <bhusal.aananda1@gmail.com>
2026-02-27 08:47:52 +0200

    feat: refactor HeroSlider component to simplify structure and update image source

--- components/Slider/HeroSlider.tsx
+++ components/Slider/HeroSlider.tsx
@@ -1,38 +1,20 @@
-import Image from "next/image"
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel"
-import { heroslides } from "@/lib/heroslides"
+import Image from "next/image";
 
 const HeroSlider = () => {
-    return(
-        <section className="relative w-full">
-      <Carousel className="w-full">
-        <CarouselContent>
-          {heroslides.map((slide) => (
-            <CarouselItem key={slide.id}>
-             <div className="relative h-[35vh] md:h-[80vh]  lg:h-[85vh] w-full">
-                <Image
-                  src={slide.image}
-                  alt={slide.title}
-                  fill
-                  priority
-                  className="object-cover"
-                />
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <h2 className="text-white text-4xl md:text-6xl font-bold text-center px-4">
-                    {slide.title}
-                  </h2>
-                </div>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-
-        <CarouselPrevious className="hidden lg:flex left-4" />
-<CarouselNext className="hidden lg:flex right-4" />
-      </Carousel>
+  return (
+    <section className="lg:mt-20 md:mt-20 sm:mt-10 mb-30 w-full h-dvh flex justify-center">
+      <div className="relative w-[95%] md:w-[90%] lg:w-[85%] h-3/5 rounded-md overflow-hidden">
+      
+        <Image
+          src="/heroslider4.png"
+          alt="Image Slider"
+          fill
+          priority
+          className="object-cover"
+        />
+      </div>
     </section>
-
-    )
-}
+  );
+};
 
 export default HeroSlider;
