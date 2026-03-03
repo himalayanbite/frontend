1b37fbc0f217addd06fb90f05fb5e4a9aa613b3e
Aananda-git <bhusal.aananda1@gmail.com>
2026-03-03 22:15:53 +0200

    feat: hero slide items  date changed

--- lib/heroslides.ts
+++ lib/heroslides.ts
@@ -5,7 +5,7 @@ export const slides: Slides = [
   {
     id: 1,
     title: "LOUNAS BUFFET",
-    subtitle: "SPECIAL OFFER",
+    subtitle: "AVAUSTARJOUS",
     originalPrice: "13,50€",
     discountedPrice: "10€",
     description: "VOIMASSA 9.3. – 13.3.2026\nAVAJAISTARJOUS!",
@@ -17,10 +17,10 @@ export const slides: Slides = [
   {
     id: 2,
     title: "À LA CARTE",
-    subtitle: "CHEF'S SPECIALS",
+    subtitle: "AVAUSTARJOUS",
     originalPrice: "",
     discountedPrice: "-20%",
-    description: "Three-course menu with seasonal ingredients\nIncludes starter, main course & dessert",
+    description: "VOIMASSA 9.3. – 13.3.2026\nAVAJAISTARJOUS!",
     image: "/img2.jpg",
     bgColor: "from-teal-900 via-teal-700 to-teal-500",
     menuLink: "/menu",
