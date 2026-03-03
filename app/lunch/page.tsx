60a18f632983ab577fff9fb2a560da0e05e9eaf5
Aananda-git <bhusal.aananda1@gmail.com>
2026-03-03 17:16:58 +0200

    feat: add LunchPage component to display LunchBuffet

---
+++ app/lunch/page.tsx
@@ -0,0 +1,11 @@
+import LunchBuffet from "@/components/menu/LunchBuffet";
+
+const LunchPage = () => {
+    return(
+        <>
+            <LunchBuffet />
+        </>
+    )
+}
+
+export default LunchPage;
