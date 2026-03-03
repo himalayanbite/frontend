6c5241a99e505388649529fc88349c9e45c70176
Aananda-git <bhusal.aananda1@gmail.com>
2026-03-03 17:17:04 +0200

    feat: add LunchBuffet component to display lunch buffet section

---
+++ components/menu/LunchBuffet.tsx
@@ -0,0 +1,18 @@
+const LunchBuffet = () => {
+  return (
+    <section className="w-full flex justify-center pt-4 md:pt-8">
+      <div
+        className="
+        bg
+        w-full sm:w-[90%]
+        h-[55vh] sm:h-[60vh] md:h-[70vh]
+        border border-amber-400
+      "
+      >
+        <h1>Lunch Buffet</h1>
+      </div>
+    </section>
+  );
+};
+
+export default LunchBuffet;
