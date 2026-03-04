277813bb47c66eae3ab87652b510e5adc7bb538a
Aananda-git <bhusal.aananda1@gmail.com>
2026-03-04 17:40:02 +0200

    fix: add new menu item

--- data/LunchData.ts
+++ data/LunchData.ts
@@ -6,33 +6,33 @@ export const lunchData: LunchDay[] = [
     label: "Maanantai 09.03.2026",
     items: [
       "Kermainen lohikeitto (L+G)",
-      "Butter Chicken paahdetuilla cashewpähkinöillä (L+G+VS)",
-      "Makkarastroganoff smetanalla (L+G)",
-      "Grillattua parsakaalia ja kukkakaalia tofulla (VE+G)",
-      "Mango-Vanilla pannacotta (L+G)",
+      "Nepalilainen Butter Chicken paahdetuilla cashewpähkinöillä (L+G+VS+sis.pähkinää) basmatiriisi(VE+G)",
+      "Makkarastroganoff suolakurkulla ja smetanalla (L+G) voilla höystetty perunamuusi (L+G)",
+      "Grillattua parsakaalia ja kukkakaalia rapealla tofulla ja paahdetuilla kurpitsansiemenillä (VE+G)",
+      "Mango-Vanilla pannacotta (L+G)"
     ],
   },
   {
     day: "tiistai",
     label: "Tiistai 10.03.2026",
     items: [
-      "Tomaatti-vuohenjuustokeitto krutongeilla (L+G)",
-      "Lammasta masalakastikkeessa basmatiriisillä (L+G+VS)",
-      "Texmex-kanatortillat cheddarjuustolla (VL+VS)",
-      "Itse tehdyt pinaattiletut puolukkahillolla (L)",
-      "Ratatouille tofulla (VE+G+VS)",
-      "Himalayan jäätelöbaari (L+G)",
+      "Tomaatti-vuohenjuustokeitto (L+G) krutongit (VE)",
+      "Lammasta ja tuoretta korianteria mausteisessa masalakastikkeessa (L+G+VS) basmatiriisi (VE+G)",
+      "Texmex-kanatortillat cheddarjuustolla (VL+VS) paahdettu sipuli (VL) creme fraiche (L+G)",
+      "Itse tehdyt pinaattiletut (L) puolukkahillo (VE+G)",
+      "Maukas ratatouille tofulla ja herneenversoilla (VE+G+VS)",
+      "Himalayan herkullinen jäätelöbaari (L+G)"
     ],
   },
   {
     day: "keskiviikko",
     label: "Keskiviikko 11.03.2026",
     items: [
-      "Savuporokeitto (L+G)",
-      "Broilerin rintafile curry-mangokastikkeessa (L+G+VS)",
-      "Lohipihvit hollandaisekastikkeella (VL+G)",
-      "Bataatti-kukkakaalikorma tofulla (VE+G+VS)",
-      "Vadelma-kermakakku (L)",
+      "Koskenlaskija-savuporokeitto (L+G)",
+      "Mureat broilerin rintafileet curry-mangokastikkeessa (L+G+VS) basmatiriisi (VE+G)",
+      "Talon lohipihvit hollandaisekastikkeella (VL+G) tilliperunat (VE+G)",
+      "Pehmeä bataatti-kukkakaalikorma tofulla ja kookoskermalla (VE+G+VS)",
+      "Vadelma-kermakakku (L)"
     ],
   },
   {
@@ -40,22 +40,22 @@ export const lunchData: LunchDay[] = [
     label: "Torstai 12.03.2026",
     items: [
       "Savupotka-hernekeitto (M+G)",
-      "Talon lihapullat metsäsienikastikkeella (L+G)",
-      "Grillattu kanan paistileike chimichurrilla (M+G+VS)",
-      "Chana masala kikhernecurry (VE+G+VS)",
-      "Talon letut (L)",
-      "Gluteenittomat vegaaniset letut (VE+G)",
+      "Talon lihapullat karamellisoidulla sipulilla ja metsäsienikastikkeella (L+G) timjamiperunat (VE+G)",
+      "Grillattua kanan paistileikettä chimichurri-glaseerauksella (M+G+VS) basmatiriisi (VE+G) tomaatti-ananassalsa (VE+G+VS)",
+      "Chana masala - mausteinen kasvis-kikhernecurry tofulla (VE+G+VS) basmatiriisi (VE+G)",
+      "Talon letut (L) oman keittiön mansikkahillo (VE+G) vaniljavaahto (L+G) jäätelo (L+G)",
+      "Gluteenittomat vegaaniset letut (VE+G)"
     ],
   },
   {
     day: "perjantai",
     label: "Perjantai 13.03.2026",
     items: [
-      "Pinaattikeitto (L+G)",
-      "Korealainen BBQ-possua (M+G+VS)",
-      "Chicken 65 (M+G+VS)",
-      "Bataatti- ja porkkanalohkot tofulla (VE+G+VS)",
-      "Snickers-mousse (L+G)",
+      "Pinaattikeitto (L+G) keitetty kananmuna",
+      "Korealaisella BBQ kastikkeella grillattua possua ja tuoreella kevätsipulilla (M+G+VS) uunissa paahdetut lohkoperunat (VE+G)",
+      "Mausteinen nepalilainen chicken 65 (M+G+VS) basmatiriisi (VE+G) kurkkuraita (L+G)",
+      "Chilillä rikastetut bataatti- ja porkkanalohkot tofulla ja edamamepavuilla (VE+G+VS)",
+      "Snickers-mousse (L+G+sis.pähkinää)"
     ],
   },
 ];
