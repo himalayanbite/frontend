c1dcec62ab91583b139975daac098f4022f2172f
Aananda-git <bhusal.aananda1@gmail.com>
2026-03-17 19:01:55 +0200

    menu

--- data/LunchData.ts
+++ data/LunchData.ts
@@ -5,58 +5,58 @@ export const lunchData: LunchDay[] = [
     day: "maanantai",
     label: "Maanantai 16.03.2026",
     items: [
-      "Kermainen lohikeitto (L,G)",
-      "Nepalilainen Chicken Tikka Masala kastikkeessa (L,G,VS) basmatiriisi (VE,G)",
-      "Talon makkara pyttipannua paistetulla kananmunalla (M,G) sinappikurkkusalaatti (VE,G)",
-      "Paneer Tikka - paistettua paneer juustoa, paprikaa, sipulia ja tomaattia jogurttikastikkeessa (VL,G,VS)",
-      "Grillatut tuoreet kasvikset ja tofua Sweet chili -kastikkeella (VE,G,VS)",
-      "Mangolassi kardemummalla ja tuoreella mintulla (L,G)"
+      "Kermainen lohikeitto (L.G)",
+      "Chicken Tikka Masala (L.G.VS) basmatiriisi (VE.G)",
+      "Talon makkara pyttipannulla ja paistettu kananmuna (M.G)",
+      "Paneer Tikka jogurttikastikkeessa (VL.VS)",
+      "Grillatut kasvikset ja tofu sweet chili -kastikkeella (VE.G.VS)",
+      "Mangolassi kardemummalla ja mintulla (L.G)"
     ],
   },
   {
     day: "tiistai",
     label: "Tiistai 17.03.2026",
     items: [
-      "Bataatti-savuchilisosekeitto (L,G,VS) krutongit (VE)",
-      "Lammasta pinaatti-valkosipuli-kookoskermakastikkeessa (L,G,VS) basmatiriisi (VE,G)",
-      "Tex Mex -kana burritot meksikolaiseen tapaan cheddarjuustolla (VL,VS) tomaattisalsa (VE,G,VS)",
-      "Itse tehdyt pinaattiletut (L) puolukkahillo (VE,G)",
-      "Paistetut  kasvikset ja soijapala satay-kuorrutuksella (VE,G,VS,sis.pähkinää)",
-      "Himalayan herkullinen jäätelöbaari (L,G)"
+      "Bataatti–savuchili sosekeitto ja krutongit (L.G)",
+      "Lammas pinaatti–valkosipuli–kookoskermakastikkeessa (L.G.VS) basmatiriisi (VE.G)",
+      "Tex Mex -kana burrito cheddarilla ja tomaattisalsalla (VL)",
+      "Itse tehdyt pinaattiletut (L) puolukkahillo (VE.G)",
+      "Paistetut kasvikset ja soijapala satay-kastikkeella (VE.G.VS)",
+      "Himalayan Ice-cream bar (L)"
     ],
   },
   {
     day: "keskiviikko",
     label: "Keskiviikko 18.03.2026",
     items: [
-      "Tom yum -rapukeitto (M,G,VS)",
-      "Mureat broilerin rintafileet curry-vindalokastikkeessa (L,G,VS) basmatiriisi (VE,G)",
-      "Talon bolognesekastike (M,G,VS) yrtti-spaghetti (L) parmesaanimuru (VL,G)",
-      "Grillatut Teriyaki-kasvikset tofulla ja tuoreella korianterilla (VE,G,VS)",
-      "Talon vadelma-mustikkapiirakka (L) ja vaniljakastike (L,G)"
+      "Tom Yum -rapukeitto (M.G.VS)",
+      "Broilerin rintafile curry-vindalokastikkeessa (M.G.VS) basmatiriisi (VE.G)",
+      "Talon bolognese (M.G.VS) yrtti-spagetti parmesaanilla (VL)",
+      "Teriyaki-kasvikset tofulla ja korianterilla (VE.G.VS)",
+      "Vadelma–mustikkapiirakka ja vaniljakastike (L)"
     ],
   },
   {
     day: "torstai",
     label: "Torstai 19.03.2026",
     items: [
-      "Savupotka-hernekeitto (M,G)",
-      "Friteerattuja kalafileitä, tomaattia, sipulia ja tuoretta paprikaa chilikastikkeessa (L,G,VS) basmatiriisi (VE,G)",
-      "Talon lihapullat mummon ruskeassa kastikkeessa (L,G) rosmariini perunat (VE,G)",
-      "Nepalilainen kukkakaali- ja tofumasala tuoreella babypinaatilla (VE,G,VS)",
-      "Talon letut (L) oman keittiön mansikkahillo (VE,G) vaniljavaahto (L,G) jäätelö (L,G)",
-      "Gluteenittomat vegaaniset letut (VE,G)"
+      "Hernekeitto savupotkalla (M.G)",
+      "Friteerattu kalafile chilikastikkeessa (M.G.VS) basmatiriisi (VE)",
+      "Talon lihapullat ruskeassa kastikkeessa (L.G.VS) rosmariiniperunat (VE)",
+      "Nepalilainen kukkakaali–tofumasala babypinaatilla (VE.G.VS)",
+      "Letut (L) mansikkahillolla (VE.G), vaniljavaahdolla ja jäätelöllä (L.G)",
+      "Gluteenittomat vegaaniset letut"
     ],
   },
   {
     day: "perjantai",
     label: "Perjantai 20.03.2026",
     items: [
-      "Samettinen kukkakaalikeitto (L,G)",
-      "Talon paneroitu currylla maustettu kanan rintafilettä (L,G,VS) lohkoperunat (VE,G) kurkkuraita (L,G)",
-      "Aasialaista karamellipossua tuoreella korianterilla (M,G,VS) basmatiriisi (VE,G)",
-      "Täyteläinen Butter tofu tuoreilla kasviksilla (VE,G,sis.pähkinää)",
-      "Mustikkarahka (L,G) granola (M)"
+      "Samettinen kukkakaalikeitto (L.G)",
+      "Talon paneroitu currymaustettu broilerin rintafile (M.G.VS) lohkoperunat (VE.G) aioli (L.VS)",
+      "Aasialainen karamellipossu (M.G.VS) basmatiriisi (VE.G)",
+      "Butter tofu tuoreilla kasviksilla (VE.G.VS.sis.pähkinä)",
+      "Mustikkarahka ja granola (L.G)"
     ],
   },
 ];
