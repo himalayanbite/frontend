381b93c4ffd607d50d245c06d7fda24d969041ba
Aananda-git <bhusal.aananda1@gmail.com>
2026-05-04 09:20:21 +0300

    update OpeningPopup component: clear image source for better flexibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <OpeningPopup imageSrc="/vappu.png" />
+        <OpeningPopup imageSrc="" />
         {children}
 
         <Footer />
